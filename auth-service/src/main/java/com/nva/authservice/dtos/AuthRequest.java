9cea50a1621316276b49923a56d5e61036b46f45
Anh Nguyen Van <anhnv.forwork@gmail.com>
2024-03-12 09:07:08 +0700

    feat: implement auth service

--- auth-service/src/main/java/com/nva/authservice/dtos/AuthRequest.java
+++ auth-service/src/main/java/com/nva/authservice/dtos/AuthRequest.java
@@ -1,14 +1,12 @@
 package com.nva.authservice.dtos;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class AuthRequest {
     private String email;
     private String password;
